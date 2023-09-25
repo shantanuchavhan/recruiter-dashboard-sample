d43862d2f016356a629d5dfeb0a2c3dee44ce802
shantanu <shantanuchavhan10@gmail.com>
2023-09-25 11:47:02 +0530

    commit

--- task-2/src/App.tsx
+++ task-2/src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './components/Header';
+import MainBody from './components/MainBody';
+import SideBar from './components/SideBar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className="sidebar">
+        <SideBar/>
+       </div>
+        <div className="header">
+          <Header/>
+        </div>
+        <div className="main">
+          <MainBody/>
+        </div>
     </div>
   );
 }
